cf3b4f379a4b280407f9d0daf1eb4f33c7887cd0
Narriman Lima <narrimancslima@gmail.com>
2022-07-13 18:32:43 -0300

    Feat: Data Context

---
+++ EFCoreRelationships/Context/SqlContext.cs
@@ -0,0 +1,21 @@
+﻿using EFCoreRelationships.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreRelationships.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Character> Characters { get; set; }
+
+        public DbSet<Weapon> Weapons { get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
